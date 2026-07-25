31dba5d3a50c8ffac188e6721c3885f6394d4a0a
Abdul Mannan <abdulmannan010spn@gmail.com>
2026-07-25 16:05:05 +0530

    contact Page

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,33 @@
+import { openingHours, storeInfo} from "../../Constants";
+
+const Contact = () => {
+  return (
+    <footer id="contact">
+      <div className="content">
+        <h2 className="font-[DM_Serif_Display]">{storeInfo.heading}</h2>
+
+        <div>
+          <h3>Visit Our Store</h3>
+          <p>{storeInfo.address}</p>
+        </div>
+
+        <div>
+          <h3>Contact Us</h3>
+          <p>{storeInfo.contact.phone}</p>
+          <p>{storeInfo.contact.email}</p>
+        </div>
+
+        <div>
+          <h3>Open Everyday</h3>
+          {openingHours.map((hours) => (
+            <p key={hours.day}>
+              {hours.day}: {hours.time}
+            </p>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Contact;
